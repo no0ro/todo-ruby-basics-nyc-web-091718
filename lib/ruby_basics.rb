c6457d1c15ad27353ae8f747ee5674799b743a11
Noelle Rolwes <nrolwes@gmail.com>
2018-09-07 13:00:10 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,28 +1,26 @@
 def division(num1, num2)
   sum = num1 / num2
- 
 end
 
 def assign_variable(value)
-  
+  var_name = value
 end
 
-def argue
-  
+def argue (argument1)
+  argument1
 end
 
-def greeting
-
+def greeting(greeting, name)
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping=cheese)
+  topping
 end
